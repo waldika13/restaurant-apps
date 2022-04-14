76a5e8b9f1ff64d1c02aac9a61f3c1eeaa63c1b1
waldika <waldika.irawan00@gmail.com>
2022-04-14 11:58:06 +0700

    Eslint

--- src/scripts/utils/dark-mode.js
+++
@@ -1,9 +0,0 @@
-const content = document.getElementsByTagName('body');
-const darkMode = document.getElementById('dark-change');
-
-darkMode.addEventListener('click', () => {
-  darkMode.classList.toggle('active');
-  content.classList.toggle('night');
-});
-
-export default darkMode;
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable class-methods-use-this */
 import DrawerInitiator from '../utils/drawer-initiator';
 import UrlParser from '../routes/url-parser';
 import routes from '../routes/routes';
